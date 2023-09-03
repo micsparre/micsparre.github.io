539c50f0b0eee716e210eef5e6cf8ed235608593
Michael Sparre <67245304+micsparre@users.noreply.github.com>
2023-09-03 14:11:08 -0700

    added hubot sans font and refactored assets folder structure

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,9 +1,8 @@
 // SocialLinks.tsx
 import React from 'react';
-import githubIcon from '../assets/GitHub-Icon.png'; // Relative path to the GitHub icon image
-import linkedinIcon from '../assets/LinkedIn-Icon.png'; // Relative path to the LinkedIn icon image
-import instagramIcon from '../assets/Instagram-Icon.png'; // Relative path to the Instagram icon image
-import resumeIcon from '../assets/Resume-Icon.png'; // Relative path to the Resume icon image
+import githubIcon from '../assets/icons/GitHub-Icon.png'; // Relative path to the GitHub icon image
+import linkedinIcon from '../assets/icons/LinkedIn-Icon.png'; // Relative path to the LinkedIn icon image
+import instagramIcon from '../assets/icons/Instagram-Icon.png'; // Relative path to the Instagram icon image
 import '../styles/SocialLinks.css'; // Create a separate CSS file for the icons
 
 
