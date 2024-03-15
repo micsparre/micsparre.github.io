a9c10f9b4c5862710edb5da42d2dcf6ac39e2252
Michael Sparre <micsparre@gmail.com>
2024-03-15 15:10:36 -0700

    update linkedin profile link

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,21 +1,32 @@
 // SocialLinks.tsx
-import React from 'react';
-import githubIcon from '../assets/icons/GitHub-Icon.png'; // Relative path to the GitHub icon image
-import linkedinIcon from '../assets/icons/LinkedIn-Icon.png'; // Relative path to the LinkedIn icon image
-import instagramIcon from '../assets/icons/Instagram-Icon.png'; // Relative path to the Instagram icon image
-import '../styles/SocialLinks.css'; // Create a separate CSS file for the icons
-
+import React from "react";
+import githubIcon from "../assets/icons/GitHub-Icon.png"; // Relative path to the GitHub icon image
+import linkedinIcon from "../assets/icons/LinkedIn-Icon.png"; // Relative path to the LinkedIn icon image
+import instagramIcon from "../assets/icons/Instagram-Icon.png"; // Relative path to the Instagram icon image
+import "../styles/SocialLinks.css"; // Create a separate CSS file for the icons
 
 const SocialLinks: React.FC = () => {
   return (
     <div className="social-links">
-      <a href="https://github.com/micsparre" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://github.com/micsparre"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={githubIcon} alt="GitHub" />
       </a>
-      <a href="https://linkedin.com/in/michaelthorsparre" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://linkedin.com/in/micsparre"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={linkedinIcon} alt="LinkedIn" />
       </a>
-      <a href="https://instagram.com/micsparre" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://instagram.com/micsparre"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={instagramIcon} alt="Instagram" />
       </a>
     </div>
